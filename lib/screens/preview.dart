428f5d8299a768f0254993dc52c90b27f8bd72c9
Roberto Vidal <42395136+robertov1@users.noreply.github.com>
2021-07-05 17:06:29 -0400

    Msg (#8)
    * redirect to chats when user sends a message from details
    
    * camera working but stretchy
    
    * Add camera with arrows
    
    * Add maps and cameraview
    
    * Connect with backend coordenates
    
    Co-authored-by: Pablo Garnham <pgarnham@uc.cl>

---
+++ lib/screens/preview.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PreviewScreen extends StatefulWidget {
+  final String imgPath;
+  final String fileName;
+  PreviewScreen({this.imgPath, this.fileName});
+
+  @override
+  _PreviewScreenState createState() => _PreviewScreenState();
+}
+
+class _PreviewScreenState extends State<PreviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+        ),
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Image.file(
+                  File(widget.imgPath),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: double.infinity,
+                  height: 60,
+                  color: Colors.black,
+                  child: Center(
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.share,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
